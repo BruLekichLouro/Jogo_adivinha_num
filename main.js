c56b3968820b4b80313cac330c5b6b4e827ce49a
BruLekichLouro <bububio@yahoo.com.br>
2023-11-15 18:31:57 -0300

    Adicionando loop de repetição para abrir o prompt até a pessoa acertar o número

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
- var numSecreto = 581;
- var chute = prompt("Digite um número entre 1 e 1000");
-
- if(chute == numSecreto){
-    alert('Acertou!')
- }else{
-    alert('Tente novamente')
+ var numSecreto = parseInt(Math.random()*1001 + 1);
+ 
+ while(chute != numSecreto){
+    var numchutes=0;
+    var chute = prompt("Digite um número entre 0 e 1000");
+    if(chute == numSecreto){
+        alert('Acertou! ')
+     }else if(chute > numSecreto){
+          
+        alert('O chute foi maior que o número secreto, tente novamente ');
+     }else if(chute < numSecreto){
+         
+        alert('O chute foi menor que o número secreto, tente novamente ' );
+     }
+   
  }
+ 
 
